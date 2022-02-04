9ae2523f051fbe4e0107e60ab8264d8a344bbabc
Lixun Zhang <lxzhang@gmail.com>
2022-02-04 20:36:20 +0000

    Adjust parameters in demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -2,29 +2,66 @@ import time
 import bisect
 import cv2 as cv
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import gridspec
 
 
-VIDEO_FILE = 'la_luna.mp4'
-RL_BITRATE_FILE = './rl_bitrate'
-RL_BUFFER_FILE = './rl_buffer'
-MPC_BITRATE_FILE = './mpc_bitrate'
-MPC_BUFFER_FILE = './mpc_buffer'
-TRACE_FILE = './network_trace'
-SKIP_FRAMES = 8820
-TOTAL_FRAMES = 2000
+VIDEO_FILE = 'bunny.mp4' # 'la_luna.mp4'
+data_dir = "./data/"
+session = "PL01-4207998440a92c1c.log" # "PL01-4207998440a92c1c.log", "PL01-7f8a5a0ab378d455.log", "PL01-9c16d837e409887d.log", "PL01-7cb0405aa4b25f4d.log"
+RL_BITRATE_FILE = data_dir + session + "_rl_bitrate"
+RL_BUFFER_FILE = data_dir + session + '_rl_buffer'
+MPC_BITRATE_FILE = data_dir + session + '_bb_bitrate'
+MPC_BUFFER_FILE = data_dir + session + '_bb_buffer'
+TRACE_FILE = data_dir + session + '_rl_network_trace'
+demo_file = session + '_demo.mp4'
+# RL_BITRATE_FILE = './rl_bitrate'
+# RL_BUFFER_FILE = './rl_buffer'
+# MPC_BITRATE_FILE = './bb_bitrate'
+# MPC_BUFFER_FILE = './bb_buffer'
+# TRACE_FILE = './network_trace'
+SKIP_FRAMES = 200 # 8820
+TOTAL_FRAMES = 1200 # 2000, better: 15 * seconds (x-axis in plot)
 CHUNK_LEN = 4.0
-ALL_BITRATES = {0.3, 0.75, 1.2}
+# ALL_BITRATES = {0.3, 0.75, 1.2}
+# ALL_BITRATES = {300.0, 750.0, 1200.0, 1850.0, 2850.0, 4300.0}
+ALL_BITRATES = {750.0, 2500.0, 4500.0, 6000.0, 8000.0, 20000.0}
 
 cap = cv.VideoCapture(VIDEO_FILE)
 kern_map = {0.3: np.ones((12, 12), np.float32) / 144, 
 			0.75: np.ones((6, 6), np.float32) / 36, 
 			1.2: np.ones((1, 1), np.float32) / 1}
+kern_map = {300.0: np.ones((15, 15), np.float32) / 225, 
+			750.0: np.ones((12, 12), np.float32) / 144, 
+			1200.0: np.ones((9, 9), np.float32) / 81, 
+			1850.0: np.ones((6, 6), np.float32) / 36, 
+			2850.0: np.ones((3, 3), np.float32) / 9, 
+			4300.0: np.ones((1, 1), np.float32) / 1}
+kern_map = {750.0: np.ones((15, 15), np.float32) / 225, 
+			2500.0: np.ones((12, 12), np.float32) / 144, 
+			4500.0: np.ones((9, 9), np.float32) / 81, 
+			6000.0: np.ones((6, 6), np.float32) / 36, 
+			8000.0: np.ones((3, 3), np.float32) / 9, 
+			20000.0: np.ones((1, 1), np.float32) / 1}
+
 text_map = {0.3: '240P',
 			0.75: '360P',
 			1.2: '720P'}
+text_map = {300.0: '240P',
+			750.0: '360P',
+			1200.0: '480P',
+			1850.0: '720P',
+			2850.0: '1080P',
+			4300.0: '1440P'}
+text_map = {750.0: '240P',
+			2500.0: '360P',
+			4500.0: '480P',
+			6000.0: '720P',
+			8000.0: '1080P',
+			20000.0: '1440P'}
 
 def read_file(FILE_NAME):
 	ts = []
@@ -61,7 +98,9 @@ for f in xrange(TOTAL_FRAMES):
 		processed_frame = cv.filter2D(frame, -1, kern_map[br])
 		all_processed_frames[br].append(processed_frame)
 
-frame = all_processed_frames[1.2][0]
+# frame = all_processed_frames[1.2][0]
+frame = all_processed_frames[2500.0][0]
+
 
 fig = plt.figure(figsize=(14, 6))
 
@@ -78,39 +117,43 @@ img1 = ax1.imshow(frame)
 ax1.set_ylabel('RL', size=21, color='#f23030')
 ax1.xaxis.set_tick_params(bottom='off', labelbottom='off') 
 ax1.yaxis.set_tick_params(left='off', labelleft='off')
-text1 = ax1.text(1150, 650, "240P", color="white", ha="center", va="center", size=16, bbox=bbox_props)
+text1 = ax1.text(1760, 1000, "240P", color="white", ha="center", va="center", size=16, bbox=bbox_props)
 
 img2 = ax2.imshow(frame)
-ax2.set_ylabel('MPC', size=21, color='#2127dd')
+ax2.set_ylabel('BB', size=21, color='#2127dd')
 ax2.xaxis.set_tick_params(bottom='off', labelbottom='off') 
 ax2.yaxis.set_tick_params(left='off', labelleft='off')
-text2 = ax2.text(1150, 650, "240P", color="white", ha="center", va="center", size=16, bbox=bbox_props)
+text2 = ax2.text(1760, 1000, "240P", color="white", ha="center", va="center", size=16, bbox=bbox_props)
 
 ax3.plot(rl_buffer_ts, rl_buffers, color='#f23030')
-bar1, = ax3.plot([0, 0], [0, 25], color="orange", alpha=0.5)
+bar1, = ax3.plot([0, 0], [0, 45], color="orange", alpha=0.5)
 ax3.set_ylabel('RL buffer (sec)')
-ax3.set_xlim([-5, 105])
-ax3.set_ylim([-2, 26])
+ax3.set_xlim([-5, 120])
+ax3.set_ylim([-2, 46])
 ax3.xaxis.set_tick_params(labelbottom='off') 
 # ax3.yaxis.set_tick_params(labelleft='off') 
 
 ax4.plot(trace_ts, trace_bw, color='#1c1c1c', alpha=0.9)
 bar2, = ax4.plot([0, 0], [0.4, 2.3], color="orange", alpha=0.5)
 ax4.set_ylabel('Throughput (mbps)')
-ax4.set_xlim([-5, 105])
+ax4.set_xlim([-5, 120])
 ax4.xaxis.set_tick_params(labelbottom='off') 
 # ax4.yaxis.set_tick_params(labelleft='off') 
 
 ax5.plot(mpc_buffer_ts, mpc_buffers, color='#2127dd')
-bar3, = ax5.plot([0, 0], [0, 25], color="orange", alpha=0.5)
-ax5.set_ylabel('MPC buffer (sec)')
-ax5.set_xlim([-5, 105])
-ax3.set_ylim([-2, 26])
+bar3, = ax5.plot([0, 0], [0, 45], color="orange", alpha=0.5)
+ax5.set_ylabel('BB buffer (sec)')
+ax5.set_xlim([-5, 120])
+ax3.set_ylim([-2, 46])
 ax5.xaxis.set_tick_params(labelbottom='off') 
 # ax5.yaxis.set_tick_params(labelleft='off') 
 ax5.set_xlabel('Time')
 
 rolling_ts = np.linspace(0, 4 * len(rl_bitrates) - 4, len(rl_bitrates) * 20)
+# 4 * 48 = 64 * 3 
+# 4 * 30 =  40 * 3
+rolling_ts = np.linspace(0, 4 * 30, 30 * 20)
+# import pdb; pdb.set_trace()
 def get_frame_quality(rolling_ts, bitrates_ts, bitrates, buffer_ts, buffers):
 	frame_quality = {}
 	text_quality = {}
@@ -120,9 +163,8 @@ def get_frame_quality(rolling_ts, bitrates_ts, bitrates, buffer_ts, buffers):
 		br_pt = bisect.bisect(bitrates_ts, t) - 1
 		buf_pt = bisect.bisect(buffer_ts, t) - 1
 
-		if buffers[buf_pt] > 0.05:
+		if buffers[buf_pt] > 0.05: # 0.05:
 			last_frame = (last_frame + 2) % TOTAL_FRAMES
-
 		frame_quality[t] = all_processed_frames[bitrates[br_pt]][last_frame]
 		text_quality[t] = text_map[bitrates[br_pt]]
 
@@ -134,9 +176,9 @@ mpc_frame_quality, mpc_text_quality = get_frame_quality(
 	rolling_ts, mpc_bitrates_ts, mpc_bitrates, mpc_buffer_ts, mpc_buffers)
 
 def animate(i):
-	bar1.set_data([i, i], [0, 25])
+	bar1.set_data([i, i], [0, 45])
 	bar2.set_data([i, i], [0.4, 2.3])
-	bar3.set_data([i, i], [0, 25])
+	bar3.set_data([i, i], [0, 45])
 
 	img1.set_data(rl_frame_quality[i])
 	img2.set_data(mpc_frame_quality[i])
@@ -155,4 +197,6 @@ ani = animation.FuncAnimation(fig, animate, rolling_ts,
 Writer = animation.writers['ffmpeg']
 writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 
-ani.save('demo.mp4', writer=writer)
+ani.save(demo_file, writer=writer)
+# with open('demo.mp4', 'wb') as f:
+#     ani.save(f, writer=writer)
